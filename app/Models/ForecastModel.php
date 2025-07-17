9dca1460f2fdd543cf687a0edd9675f25eb1dd8e
anxietyPotato <krompirko558@gmail.com>
2025-07-17 22:50:49 +0200

    * added forecast table,model,column city_id to table forecast

---
+++ app/Models/ForecastModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ForecastModel extends Model
+{
+   protected $table = 'forecast';
+   protected $fillable = ['city_id', 'temperature', 'forecast_date'];
+}
