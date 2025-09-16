eb1e6600e72e6a2cb4442e7f97f0a098f8fa7f5b
anxietyPotato <krompirko558@gmail.com>
2025-09-16 11:48:50 +0200

    *search_result blade mopdified for better visibility of content

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -11,22 +11,29 @@
                     @foreach($cities as $city)
                         <div class="col-6 col-md-4 col-lg-3">
                             <div class="card h-100 shadow-sm bg-primary text-white">
-                                <div class="card-body d-flex flex-column align-items-center justify-content-center">
-                                    <h5 class="card-title mb-2">{{ $city->name }}</h5>
+                                <div class="card-body d-flex flex-column align-items-center justify-content-between">
+                                    <!-- City Name -->
+                                    <h5 class="card-title mb-3">{{ $city->name }}</h5>
 
                                     @php
                                         $forecast = $city->oneForecast;
                                     @endphp
 
                                     @if($forecast)
-                                        <div class="d-flex flex-column align-items-center">
-                                            <div class="fs-2 mb-1">
+                                        <!-- Mini Container for Forecast -->
+                                        <div class="w-100 p-3 rounded bg-light text-dark text-center">
+                                            <!-- Weather Icon -->
+                                            <div class="fs-2 mb-2">
                                                 {!! forecastsAdminHelper::getWeatherIcon($forecast->weather_type) !!}
                                             </div>
-                                            <div class="text-white-50">
+
+                                            <!-- Weather Type -->
+                                            <div class="fw-semibold mb-1">
                                                 {{ ucfirst($forecast->weather_type) }}
                                             </div>
-                                            <div class="mt-2"
+
+                                            <!-- Temperature -->
+                                            <div class="fw-bold"
                                                  style="color: {{ forecastsAdminHelper::getColorByTemperature($forecast->temperature) }}">
                                                 {{ $forecast->temperature }}°C
                                             </div>
@@ -44,7 +51,5 @@
             @endif
 
         </div>
-
-
     </div>
 @endsection
