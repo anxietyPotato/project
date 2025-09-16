015dec7bfb1726e4c68c0a666921233081250838
anxietyPotato <krompirko558@gmail.com>
2025-09-16 14:41:25 +0200

    *added migration usercities,added model

---
+++ database/migrations/2025_09_16_123314_create_user_cities_models_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class  CreateUserCitiesModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_cities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('city_id')->references('id')->on('cities');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_cities');
+    }
+}
