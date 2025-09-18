76d9665f48376ba63da097edf62a2a5a880fabfe
anxietyPotato <krompirko558@gmail.com>
2025-09-18 14:23:23 +0200

    *added migration usercities,added model

---
+++ app/Http/Controllers/UserCitiesController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserCitiesController extends Controller
+{
+public function favorite(Request $request) { die("tesst mdfkr"); }
+}
